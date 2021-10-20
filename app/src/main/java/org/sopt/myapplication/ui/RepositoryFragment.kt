87dd7cab792378002fab13c5b3f816c40a382b98
hotaek <kht072623@naver.com>
2021-10-21 01:06:16 +0900

    [FEAT] LEVEL1_end

--- app/src/main/java/org/sopt/myapplication/ui/RepositoryFragment.kt
+++ app/src/main/java/org/sopt/myapplication/ui/RepositoryFragment.kt
@@ -29,7 +29,8 @@ class RepositoryFragment : BaseFragment<FragmentRepositoryBinding>(R.layout.frag
             listOf(
                 RepositoryData("곽호택", "안드로이드 OB로 최선을 다합시다~~~~~더해더해"),
                 RepositoryData("곽호택", "차로를 만나서 너무 행복한 한해였습니다!! "),
-                RepositoryData("곽호택", "첫 동아리 솝트 25년 인생 최고의 선택")
+                RepositoryData("곽호택", "첫 동아리 솝트 25년 인생 최고의 선택"),
+                RepositoryData("곽호택", "안드로이드몬스터 불꽃 카리스마")
             )
         )
     }
