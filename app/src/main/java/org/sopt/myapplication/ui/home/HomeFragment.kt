502b02404def5e6d95df26249753daafffe59056
hotaek <kht072623@naver.com>
2021-10-27 18:05:27 +0900

    [FEAT] tablayout

--- app/src/main/java/org/sopt/myapplication/ui/home/HomeFragment.kt
+++ app/src/main/java/org/sopt/myapplication/ui/home/HomeFragment.kt
@@ -5,11 +5,36 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.tabs.TabLayoutMediator
 import org.sopt.myapplication.R
 import org.sopt.myapplication.databinding.FragmentHomeBinding
+import org.sopt.myapplication.ui.adapter.HomeFragmentViewPagerAdapter
 import org.sopt.myapplication.ui.base.BaseFragment
 
 
 class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
+    private lateinit var homeFragmentViewPagerAdapter: HomeFragmentViewPagerAdapter
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        initAdapter()
+        initTab()
+    }
 
+
+
+    private fun initAdapter(){
+        val fragmentList = listOf(HomeFollowingFragment(),HomeFollowerFragment())
+
+        homeFragmentViewPagerAdapter = HomeFragmentViewPagerAdapter(this)
+        homeFragmentViewPagerAdapter.fragments.addAll(fragmentList)
+
+        binding.vpHomeTab.adapter = homeFragmentViewPagerAdapter
+    }
+
+    private fun initTab(){
+        val tabLabel = listOf("첫 번째", "두 번쨰")
+
+        TabLayoutMediator(binding.tlHome, binding.vpHomeTab) {tab, position ->
+            tab.text = tabLabel[position]
+        }.attach()
+    }
 }
